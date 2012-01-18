c6135719b27be144b11ab7bf06cbe1772fb81992
matt chisholm <matt@theory.org>
2012-01-19 00:23:35 +0100

    renaming functions and moving relatively_prime() test inside primes() iterator

--- sieve.py
+++ sieve.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 import sys
 
-
-def is_prime(n, primes):
-    for p, p2 in primes:
-        if n < p2:
-            return True # is prime because if it were not, we would have hit a factor by now
-        elif n % p == 0:
-            return False # is not prime because modulo p == 0
-    return True # primes is empty so this is the first prime, 2
-
-
-def iterative_sieve():
+def primes():
     n = 1
     primes = []
+
+    def relatively_prime(n):
+        for p, p2 in primes:
+            if n < p2:
+                return True # is prime because if it were not, we would have hit a factor by now
+            elif n % p == 0:
+                return False # is not prime because modulo p == 0
+        return True # primes is empty so this is the first prime, 2
+
     while True:
         n += 1
-        if is_prime(n, primes):
+        if relatively_prime(n):
             yield n
             primes.append((n, n**2))
 
-
 def factor(n):
-    for p in iterative_sieve():
+    for p in primes():
         if p > n: 
             break
         while n % p == 0:
@@ -33,4 +31,4 @@ def factor(n):
 
 
 for arg in sys.argv[1:]:
-    print arg + ': '+ ' '.join(map(str, factor(int(arg))))
+    print arg + ': ' + ' '.join(map(str, factor(int(arg))))
